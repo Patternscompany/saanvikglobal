c78178c6c56917bf68aedfe7bf16dbb70c7d879e
Chandrakanthkokkiripati <chandrakanthkokkiripati1998@gmail.com>
2025-01-08 18:21:01 +0530

    c

--- js/main.js
+++ js/main.js
@@ -1,172 +1,165 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(window).width() > 992) {
-            if ($(this).scrollTop() > 45) {
-                $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
-            } else {
-                $('.sticky-top .container').removeClass('shadow-sm').css('max-width', $('.topbar .container').width());
-            }
-        } else {
-            $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
-        }
-    });
-
-
-    // Hero Header carousel
-    $(".header-carousel").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: false,
-        loop: true,
-        margin: 0,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ]
-    });
-
-
-
-    // Project carousel
-    $(".project-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : false,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
-
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : false,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(window).width() > 992) {
+      if ($(this).scrollTop() > 45) {
+        $(".sticky-top .container")
+          .addClass("shadow-sm")
+          .css("max-width", "100%");
+      } else {
+        $(".sticky-top .container")
+          .removeClass("shadow-sm")
+          .css("max-width", $(".topbar .container").width());
+      }
     } else {
-        $('.back-to-top').fadeOut('slow');
+      $(".sticky-top .container")
+        .addClass("shadow-sm")
+        .css("max-width", "100%");
     }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  });
 
+  // Hero Header carousel
+  $(".header-carousel").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+  });
 
-    // On page load, check for the stored language preference in localStorage
-    window.addEventListener('load', function () {
-        const savedLang = localStorage.getItem('language');
-        if (savedLang) {
-            changeLanguage(savedLang);
-        } else {
-            // Default to English if no language is saved
-            changeLanguage('en');
-        }
-    });
+  // Project carousel
+  $(".project-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav: false,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
 
-    // Language switch functionality
-    document.getElementById('languageSwitch').addEventListener('click', function () {
-        const currentLang = this.textContent.includes('Dutch') ? 'nl' : 'en';
-        localStorage.setItem('language', currentLang); // Save the language choice
-        changeLanguage(currentLang);
-    });
+  // testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav: false,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
 
-    // Function to change content based on language
-    function changeLanguage(language) {
-        const elements = document.querySelectorAll('[data-lang-en]'); // Elements with translations
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 5,
+    time: 2000,
+  });
 
-        elements.forEach(el => {
-            el.textContent = language === 'nl' ? el.getAttribute('data-lang-nl') : el.getAttribute('data-lang-en');
-        });
-
-        const langButton = document.getElementById('languageSwitch');
-        langButton.textContent = language === 'nl' ? 'English' : 'Dutch';
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
     }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
+  window.addEventListener("load", function () {
+    const savedLang = localStorage.getItem("language") || "en";
+    changeLanguage(savedLang);
+  });
 
+  function switchLanguage(language) {
+    localStorage.setItem("language", language);
+    changeLanguage(language);
+  }
+
+  function changeLanguage(language) {
+    const elements = document.querySelectorAll("[data-lang-en]");
+    elements.forEach((el) => {
+      el.textContent =
+        language === "en"
+          ? el.getAttribute("data-lang-en")
+          : el.getAttribute("data-lang-nl");
+    });
+
+    const langButton = document.getElementById("languageSwitch");
+    const langIcon = document.getElementById("currentLangIcon");
+
+    if (language === "nl") {
+      langButton.innerHTML = `Dutch`;
+    } else {
+      langButton.innerHTML = `English`;
+    }
+  }
 })(jQuery);
-
